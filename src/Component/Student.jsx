96ec7725c6935c58c52ab98d016fbf8d824bd872
OdeToCode147 <109613172+OdeToCode147@users.noreply.github.com>
2022-11-04 19:32:19 +0530

    Update Student.jsx

--- src/Component/Student.jsx
+++ src/Component/Student.jsx
@@ -13,6 +13,7 @@ const Student = () => {
           <th>Name</th>
           <th>Course</th>
           <th>Batch</th>
+          <th>Change</th>
         </tr>
 
         <hr />
@@ -21,6 +22,7 @@ const Student = () => {
           <td>Satya</td>
           <td>Computer Science</td>
           <td>August</td>
+          <td className="Edit">Edit</td>
         </tr>
 
         <hr />
@@ -28,8 +30,9 @@ const Student = () => {
         <tr>
           <td>23</td>
           <td>Surya</td>
-          <td>EEE</td>
+          <td>Electrical and Electronics</td>
           <td>September</td>
+          <td className="Edit">Edit</td>
         </tr>
 
         <hr />
@@ -39,6 +42,45 @@ const Student = () => {
           <td>Vamsi</td>
           <td>Chemical</td>
           <td>October</td>
+          <td className="Edit">Edit</td>
+        </tr>
+        <hr />
+        <tr>
+          <td>31</td>
+          <td>Srikar</td>
+          <td>Information Technology</td>
+          <td>May</td>
+          <td className="Edit">Edit</td>
+        </tr>
+
+        <hr />
+
+        <tr>
+          <td>36</td>
+          <td>Charan</td>
+          <td>Electronics and Communication</td>
+          <td>January</td>
+          <td className="Edit">Edit</td>
+        </tr>
+
+        <hr />
+
+        <tr>
+          <td>42</td>
+          <td>Ruthwik</td>
+          <td>Civil</td>
+          <td>June</td>
+          <td className="Edit">Edit</td>
+        </tr>
+
+        <hr />
+
+        <tr>
+          <td>48</td>
+          <td>Saketh</td>
+          <td>Mechatronics</td>
+          <td>April</td>
+          <td className="Edit">Edit</td>
         </tr>
       </table>
     </div>
